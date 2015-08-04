be7b142b4bb5bd72445c68740333f5153d926c37
Jiri Bakker <jiri.bakker@infi.nl>
2015-08-04 23:00:39 +0200

    Basic F# implementation for a tennis game (without tie-breaks for now)

---
+++ Jiri.FSharp/Jiri.FSharp.TennisGame.UnitTests/TennisGameTests.fs
@@ -0,0 +1,259 @@
+﻿namespace Jiri.FSharp.TennisGame.UnitTests
+open System.Collections.Generic
+open NUnit.Framework
+open FsUnit
+open Jiri.FSharp.TennisGame
+
+[<TestFixture>]
+type TennnisGameTests() = 
+
+    let player1 = new Player("Player 1")
+    let player2 = new Player("Player 2")
+    
+    let startTennisGame() =
+        new TennisGame(player1, player2)
+
+    let scoreInGame player gameState =
+        gameState.CurrentGame.[player]
+
+    let gamesWonInSet player gameState =
+        gameState.Sets.Head.Games.[player]
+
+    let setsWon player gameState =
+        gameState.Sets
+        |> Seq.filter (fun (set) -> match set.Winner with | None -> false | Some(p) -> p.Key = player)
+        |> Seq.length
+       
+    let scorePointsFor player points (tennisGame:TennisGame) =
+        for i = 1 to points do
+            tennisGame.ScorePointFor player       
+
+    let winGame player tennisGame =
+        scorePointsFor player 4 tennisGame
+
+    let winGames player games tennisGame =
+        for i = 1 to games do
+            winGame player tennisGame
+
+    let getDueceGame() =
+        let tennisGame = startTennisGame()
+        tennisGame |> scorePointsFor player1 3
+        tennisGame |> scorePointsFor player2 3        
+        tennisGame
+
+    
+
+
+    [<Test>]
+    member this.InNewGameBothPlayersHaveNoPointsInGame () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Zero
+        
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Zero
+
+    [<Test>]
+    member this.FirstPointscoreInGameOneShouldGiveScore15 () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> scorePointsFor player1 1 |> ignore
+        
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Fifteen
+
+    [<Test>]
+    member this.TwoPointsscoreInGameOneShouldGiveScore30 () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> scorePointsFor player1 2
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Thirty
+
+    [<Test>]
+    member this.ThreePointsscoreInGameOneShouldGiveScore40 () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> scorePointsFor player1 3
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Forty
+
+    [<Test>]
+    member this.FourPointsscoreInGameOneShouldStartsNewGame () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> scorePointsFor player1 4        
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Zero
+
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Zero
+
+    [<Test>]
+    member this.ThreePointsForBothPlayersShouldGiveScoreDuece () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> scorePointsFor player1 3        
+        tennisGame |> scorePointsFor player2 3        
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Duece
+
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Duece
+
+    [<Test>]
+    member this.ScoreInGameOneAtDueceGivesAdvantage () =
+        let tennisGame = getDueceGame()
+
+        tennisGame |> scorePointsFor player1 1
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Advantage
+
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Forty
+
+    [<Test>]
+    member this.ScoreInGameTwoAtDueceGivesAdvantage () =
+        let tennisGame = getDueceGame()
+
+        tennisGame |> scorePointsFor player2 1
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Forty
+
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Advantage
+
+    [<Test>]
+    member this.ScoreInGameOneWhenPlayerTwoHasAdvantageGivesDuece () =
+        let tennisGame = getDueceGame()
+        
+        tennisGame |> scorePointsFor player2 1
+        tennisGame |> scorePointsFor player1 1
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Duece
+
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Duece
+
+    [<Test>]
+    member this.ScoreInGameOneAtAdvantageStartsNewGame () =
+        let tennisGame = getDueceGame()
+        
+        tennisGame |> scorePointsFor player1 1
+        tennisGame |> scorePointsFor player1 1
+
+        tennisGame.CurrentScore
+        |> scoreInGame player1
+        |> should equal Zero
+
+        tennisGame.CurrentScore
+        |> scoreInGame player2
+        |> should equal Zero
+
+    [<Test>]
+    member this.InNewGameBothPlayersHaveNogamesWonInSet () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame.CurrentScore
+        |> gamesWonInSet player1
+        |> should equal 0
+        
+        tennisGame.CurrentScore
+        |> gamesWonInSet player2
+        |> should equal 0
+
+
+    [<Test>]
+    member this.ScoreFourPointsInGameScoresGameInSet () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> scorePointsFor player1 4
+
+        tennisGame.CurrentScore
+        |> gamesWonInSet player1
+        |> should equal 1
+
+        tennisGame.CurrentScore
+        |> gamesWonInSet player2
+        |> should equal 0
+
+    [<Test>]
+    member this.WinTwoGamesScoresTwogamesWonInSet () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> winGames player1 1
+        tennisGame |> winGames player1 1
+
+        tennisGame.CurrentScore
+        |> gamesWonInSet player1
+        |> should equal 2
+
+    [<Test>]
+    member this.WinSixGamesScoresSetWon () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> winGames player1 6
+
+        tennisGame.CurrentScore
+        |> setsWon player1
+        |> should equal 1
+
+    [<Test>]
+    member this.WinSixthGameWhenOpponentHasFiveGamesDoesNotEndSet () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> winGames player2 5
+        tennisGame |> winGames player1 6
+
+        tennisGame.CurrentScore
+        |> setsWon player1
+        |> should equal 0
+
+    [<Test>]
+    member this.WinSeventhGameWhenOpponentHasFiveGamesWinsSet () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> winGames player2 5
+        tennisGame |> winGames player1 7
+
+        tennisGame.CurrentScore
+        |> setsWon player1
+        |> should equal 1
+
+    [<Test>]
+    member this.SetContinuesWhileDifferenceInGamesIsNot2 () =
+        let tennisGame = startTennisGame()
+        
+        tennisGame |> winGames player2 5
+        tennisGame |> winGames player1 5
+
+        for i = 1 to 10 do
+            tennisGame |> winGames player1 1
+            tennisGame |> winGames player2 1
+
+        tennisGame.CurrentScore
+        |> setsWon player1
+        |> should equal 0        
---
+++ Jiri.FSharp/Jiri.FSharp.TennisGame/TennisGame.fs
@@ -0,0 +1,126 @@
+﻿namespace Jiri.FSharp.TennisGame
+open System.Collections.Generic
+
+module Seq =
+    let asMap (sequence:seq<'a * 'b>) =
+        sequence |> Map.ofSeq
+
+type Player(name) = 
+    member this.Name = name
+
+    override this.Equals(otherObj) =
+        match otherObj with
+        | :? Player as y -> (this.Name = y.Name)
+        | _ -> false
+ 
+    override this.GetHashCode() = hash this.Name
+    interface System.IComparable with
+        member this.CompareTo otherObj =
+            match otherObj with
+            | :? Player as y -> compare this.Name y.Name
+            | _ -> invalidArg "yobj" "cannot compare values of different types"
+    
+type Score = Zero | Fifteen | Thirty | Forty | Duece | Advantage
+
+type Set = { Games: Map<Player,int> }
+    with member this.Winner =
+            let player1 = Seq.head this.Games
+            let player2 = Seq.head (Seq.tail this.Games)
+        
+            match player1.Value,player2.Value with
+            | a,b when a >= 6 && a-b >= 2 -> Some(player1)
+            | a,b when b >= 6 && b-a >= 2 -> Some(player2)
+            // TODO tie-breaks?
+            // | 7,6 -> Some(player1) 
+            // | 6,7 -> Some(player2)
+            | _,_ -> None
+    
+type GameState = { Sets: Set list; CurrentGame: Map<Player,Score> }
+
+type TennisGame(player1:Player, player2:Player) = 
+
+    let startNewGame =
+        Seq.asMap [(player1, Zero); (player2, Zero)]    
+
+    let startNewSet =
+        { Games = Seq.asMap [(player1,0);(player2,0)] }
+
+    let getOtherPlayer (player:Player) =        
+        match player with
+        | _ when player1.Name = player.Name -> player2
+        | _                                 -> player1 
+
+    let getCurrentSet gameState = 
+        gameState.Sets.Head
+
+    let getPreviousSets gameState =
+        gameState.Sets.Tail
+
+    let getGamesWonInSet player set =
+        set.Games.[player]        
+
+    let incrementGamesWonInSet player set =
+        let otherPlayer              = getOtherPlayer player
+        let playerGamesWonInSet      = getGamesWonInSet player set
+        let otherPlayerGamesWonInSet = getGamesWonInSet otherPlayer set
+        { Games = Seq.asMap [(player,playerGamesWonInSet+1);(otherPlayer,otherPlayerGamesWonInSet)] }
+        
+    let winGame player gameState =
+        let updatedSet = 
+            getCurrentSet gameState
+            |> incrementGamesWonInSet player
+
+        let previousSets = getPreviousSets gameState
+        match updatedSet.Winner with
+        | Some(player) -> { Sets = [startNewSet;updatedSet] @ previousSets; CurrentGame = startNewGame }
+        | None         -> { Sets = [updatedSet]             @ previousSets; CurrentGame = startNewGame }  
+
+    let incrementScoreInNormalGame player gameState =
+        let pointsInCurrentGame = gameState.CurrentGame
+        let otherPlayer         = getOtherPlayer player
+        let scoreForPlayer      = pointsInCurrentGame.[player]
+        let scoreForOtherPlayer = pointsInCurrentGame.[otherPlayer]
+
+        let (scorePlayer,scoreOtherPlayer,gameWinner) =
+            match scoreForPlayer,scoreForOtherPlayer with
+                | Zero,otherScore    -> Fifteen,otherScore,None
+                | Fifteen,otherScore -> Thirty,otherScore,None
+                | Thirty,otherScore  -> 
+                    match otherScore with
+                        | Zero
+                        | Fifteen
+                        | Thirty -> Forty,otherScore,None
+                        | Forty  -> Duece,Duece,None
+                        | _      -> invalidOp "Unexpected game state"
+                | Forty,otherScore ->
+                    match otherScore with
+                        | Zero
+                        | Fifteen
+                        | Thirty    -> Forty,otherScore,Some(player)
+                        | Advantage -> Duece,Duece,None
+                        | _         -> invalidOp "Unexpected game state"
+                | Duece,Duece     -> Advantage,Forty,None
+                | Duece,_         -> invalidOp "Unexpected game state"
+                | Advantage,Forty -> Advantage,Forty,Some(player)
+                | _,_             -> invalidOp "Unexpected game state"
+
+        match gameWinner with
+        | Some(winner) -> 
+            winGame winner gameState            
+        | None ->
+            { Sets = gameState.Sets; CurrentGame = (Seq.asMap [(player,scorePlayer);(otherPlayer,scoreOtherPlayer)]) }
+
+    let winGame player =
+        player |> ignore
+
+    let scorePointFor player gameState =
+        incrementScoreInNormalGame player gameState
+
+    let mutable gameState = { Sets = [startNewSet]; CurrentGame = startNewGame }
+
+    member this.ScorePointFor player =
+        gameState <-
+            scorePointFor player gameState            
+
+    member this.CurrentScore =
+        gameState
